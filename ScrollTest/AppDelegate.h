b9c83cbbe16b2e8f36ebec17d401b07cff35f009
Cloudox <cloudox@foxmail.com>
2015-05-28 09:27:45 +0800

    Initial Commit

---
+++ ScrollTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollTest
+//
+//  Created by mac on 15/5/28.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
